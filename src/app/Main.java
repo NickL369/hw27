1824f0d92ca55a1204c3f2a3d22173208084f066
nlobachev <nikita.lobachev@fintech-farm.com>
2025-06-22 22:55:58 +0300

    initial commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         String url = "jdbc:postgresql://localhost:5432/my_database";
         String user = "postgres";
-        String password = "qwe88132";
+        String password = "myPassword";
 
         try (Connection conn = DriverManager.getConnection(url, user, password);
              Statement stmt = conn.createStatement()) {
